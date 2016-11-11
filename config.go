fc3c8173f3ab9e61b5c7157a208918c655f10ef4
sunlei <lei.sunstar@gmail.com>
2016-11-12 00:04:08 +0800

    修改

--- config.go
+++ config.go
@@ -1,44 +1,50 @@
 package config
 
 import (
-    "os"
-    "bufio"
-    "strings"
-    "fmt"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
 )
 
-func Unmarshal(filePath string)(jsonStr string,err error){
-    return unmarshal(filePath)
+func Unmarshal(filePath string, v interface{}) (err error) {
+
+	jsonStr, err := unmarshal(filePath)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal([]byte(jsonStr), v)
+	return
 }
 
-func unmarshal(filePath string) (jsonStr string,err error) {
-    //configMap, err := readFile("/Users/heguangxiu/go/src/config/test.conf")
-    jsonStr, err = readFile(filePath)
-    if err != nil{
-        return
-    }
-    return
+func unmarshal(filePath string) (jsonStr string, err error) {
+	jsonStr, err = readFile(filePath)
+	if err != nil {
+		return
+	}
+	return
 }
 
-func readFile(filePath string)(jsonStr string ,err error){
-    f, err := os.Open(filePath)
-    if err != nil{
-        println("os open err", err)
-        return
-    }
-    defer f.Close()
-    tmpJson := []string{}
-    scanner := bufio.NewScanner(f)
-    for scanner.Scan(){
-        str := scanner.Text()
-        if strings.HasPrefix(str, "#") {
-            //fmt.Printf("注释 %s \n", str)
-            continue
-        }
-        s := strings.Split(scanner.Text(),":")
-        tmp:=fmt.Sprintf("\"%s\":%s",s[0],strings.Join(s[1:],":"))
-        tmpJson = append(tmpJson, tmp)
-    }
-    jsonStr = fmt.Sprintf("{%s}",strings.Join(tmpJson, ","))
-    return
+func readFile(filePath string) (jsonStr string, err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		println("os open err", err)
+		return
+	}
+	defer f.Close()
+	tmpJson := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		str := scanner.Text()
+		if strings.HasPrefix(str, "#") {
+			//fmt.Printf("注释 %s \n", str)
+			continue
+		}
+		s := strings.Split(scanner.Text(), ":")
+		tmp := fmt.Sprintf("\"%s\":%s", s[0], strings.Join(s[1:], ":"))
+		tmpJson = append(tmpJson, tmp)
+	}
+	jsonStr = fmt.Sprintf("{%s}", strings.Join(tmpJson, ","))
+	return
 }
--- config_test.go
+++ config_test.go
@@ -1,31 +1,22 @@
 package config
 
-import(
-    "testing"
-    "encoding/json"
-    "fmt"
+import (
+	"testing"
 )
 
 type testC struct {
-    Id  int `json:"id"`
-    IString  string `json:"i_string"`
-    IntList []int `json:"int_list"`
-    StringList []string `json:"string_list"`
+	Id         int               `json:"id"`
+	IString    string            `json:"i_string"`
+	IntList    []int             `json:"int_list"`
+	StringList []string          `json:"string_list"`
+	StrMap     map[string]string `json:"string_map"`
 }
 
 func TestConfig(t *testing.T) {
-    a := &testC{}
-    a.Id = 2
-    a.IntList = []int{1,2,3,4}
-    a.IString = "test"
-     testJosn,_ := json.Marshal(a)
-    fmt.Println(string(testJosn))
-
-    jsonStr,err := Unmarshal("/Users/heguangxiu/go/src/config/test.conf")
-    if err != nil{
-        t.Error("err %v", err)
-    }
-    C := &testC{}
-    err = json.Unmarshal([]byte(jsonStr), C)
-    fmt.Printf("testC %v \n", C)
+	c := &testC{}
+	err := Unmarshal("test.conf", c)
+	if err != nil {
+		t.Error("err %v", err)
+	}
+	t.Logf("c (%v)", c)
 }
